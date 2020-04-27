22d2448b71538042967f53ca6afa30cef9bc9308
Saksham <saksham.indurkhya@wipro.com>
2020-04-27 19:55:58 +0530

    calculator + Helloworld

---
+++ cal_operation.py
@@ -0,0 +1,7 @@
+from cal import *
+
+sum = add(5,3)
+print("sum is: ",sum)
+print("The sub of 2 number is ",sub(5,3))
+print("The mul of 2 number is ",mul(5,3))
+print("The div of 2 number is ",div(6,3))
